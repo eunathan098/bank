dcbf8fe07580c6291027194de3ec9f63c9878aa3
Nathan <145821197+eunathan098@users.noreply.github.com>
2025-03-04 21:30:19 -0300

    Criando Menu Mobile

--- dist/scriptHome.js
+++ dist/scriptHome.js
@@ -8,3 +8,23 @@ const openDespesas = document.getElementById("add-gasto");
 openDespesas.addEventListener("click", function () {
     window.location.href = "despesas.html";
 });
+// Função de abrir meni mobile 
+const iconMobile = document.getElementById("icon-abrir");
+const containerMenuMobile = document.getElementById("container-menu-mobile");
+iconMobile.addEventListener("click", function () {
+    containerMenuMobile.style.display = "block";
+});
+// Função de fechar Meu Mobile
+const iconMobileFechar = document.getElementById("icon-fechar");
+iconMobileFechar.addEventListener("click", function () {
+    containerMenuMobile.style.display = "none";
+    if (containerMenuMobile) {
+        containerMenuMobile.style.display = "none";
+    }
+});
+// Abrir display abaixo da foto de perfil
+const displayPerfil = document.getElementById("container-display-perfil");
+const imgPerfil = document.getElementById("img-perfil");
+imgPerfil.addEventListener("click", function () {
+    displayPerfil.style.display = "block";
+});
--- src/typescript/scriptHome.ts
+++ src/typescript/scriptHome.ts
@@ -11,3 +11,31 @@ const openDespesas: any = document.getElementById("add-gasto")
 openDespesas.addEventListener("click", function(){
     window.location.href = "despesas.html";
 });
+
+// Função de abrir meni mobile 
+const iconMobile = document.getElementById("icon-abrir") as HTMLElement;
+const containerMenuMobile = document.getElementById("container-menu-mobile") as HTMLElement;
+
+iconMobile.addEventListener("click", function(){
+    containerMenuMobile.style.display = "block";
+});
+
+// Função de fechar Meu Mobile
+const iconMobileFechar = document.getElementById("icon-fechar") as HTMLElement;
+
+iconMobileFechar.addEventListener("click", function(){
+    containerMenuMobile.style.display = "none";
+
+    if(containerMenuMobile){
+        containerMenuMobile.style.display = "none";
+    }
+});
+
+// Abrir display abaixo da foto de perfil
+
+const displayPerfil = document.getElementById("container-display-perfil") as HTMLElement;
+const imgPerfil = document.getElementById("img-perfil") as HTMLElement;
+
+imgPerfil.addEventListener("click", function(){
+    displayPerfil.style.display = "block";
+});
